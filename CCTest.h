7b1776e73b7f22b1b76dec26fbedd87ffa8d1d3e
kkaner <kimheejae@KimHeejaes-iMac.local>
2014-08-25 17:38:13 +0900

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by KimHeejae on 2014. 8. 25..
+//  Copyright (c) 2014년 Kiwiple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTest : UICollectionViewCell
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,31 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by KimHeejae on 2014. 8. 25..
+//  Copyright (c) 2014년 Kiwiple. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
